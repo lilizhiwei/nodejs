a943ede8923a4ce95b6d70bf98fb7542fd7b593f
lizhiwei <1548790965@qq.com>
2017-02-26 19:43:45 +0800

    上传node.js学习代码

---
+++ 教程/3，路由初步/router.js
@@ -0,0 +1,11 @@
+/**
+ * Created by 李志伟 on 2017-02-26.
+ */
+module.exports= {
+    login: function (req, res) {
+        res.write('我是login方法');
+    },
+    zhuce: function (req, res) {
+        res.write('我是zhuce方法');
+    }
+}
